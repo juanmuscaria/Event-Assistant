b932f2aed227825f6ba825df19b9f56131753f7d
juanmuscaria <juanmuscaria@gmail.com>
2021-03-21 21:49:31 -0300

    Ensure the item registry is working.

--- src/test/java/TestFakeEnviroment.java
+++ src/test/java/TestFakeEnviroment.java
@@ -1,4 +1,6 @@
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestFakeEnviroment {
@@ -10,7 +12,7 @@ public class TestFakeEnviroment {
         environment.callInsideLauncherClassLoader(new ImSorryNotSorry.Callable(){
             @Override
             void call() {
-                System.out.println(Items.diamond.getClass());
+                Assertions.assertEquals(GameRegistry.findUniqueIdentifierFor(Items.diamond).toString(), "minecraft:diamond");
             }
         }.getClass());
     }
